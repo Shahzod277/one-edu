8420b418ac38ff3d868fe53f738c9cde207e370a
Shahzod277 <shahzodozbekov7@gmail.com>
2026-01-06 18:30:27 +0500

    create

--- src/main/java/uz/raqamli_talim/oneedu/controller/ClientSystemController.java
+++ src/main/java/uz/raqamli_talim/oneedu/controller/ClientSystemController.java
@@ -2,6 +2,7 @@ package uz.raqamli_talim.oneedu.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uz.raqamli_talim.oneedu.model.ClientSystemDto;
 import uz.raqamli_talim.oneedu.model.ResponseDto;
@@ -17,18 +18,21 @@ public class ClientSystemController {
 
     // CREATE
     @PostMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseDto create(@RequestBody ClientSystemDto dto) {
         return service.create(dto);
     }
 
     // READ by ID  -> ClientSystemDto qaytaryapti (service shunaqa)
     @GetMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
     public ClientSystemDto getById(@PathVariable Long id) {
         return service.getById(id);
     }
 
     // READ by apiKey -> ClientSystemDto qaytaryapti
     @GetMapping("/by-api-key/{apiKey}")
+    @PreAuthorize("isAuthenticated()")
     public ClientSystemDto getByApiKey(@PathVariable String apiKey) {
         return service.getByApiKey(apiKey);
     }
@@ -36,6 +40,7 @@ public class ClientSystemController {
     // READ ALL (PAGE) -> Page qaytaryapti
     // page ni siz service’da 1 dan boshlab qabul qilyapsiz (page>0 bo‘lsa -1)
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public Page<ClientSystemDto> getAllAsPage(
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "20") int size
@@ -45,6 +50,7 @@ public class ClientSystemController {
 
     // UPDATE
     @PutMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseDto update(@PathVariable Long id,
                               @RequestBody ClientSystemDto dto) {
         return service.update(id, dto);
@@ -52,6 +58,7 @@ public class ClientSystemController {
 
     // DELETE (soft)
     @DeleteMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseDto deactivate(@PathVariable Long id) {
         return service.deactivate(id);
     }
