14c8c240f7701314ac4d70a8352654212576dd89
Shahzod277 <shahzodozbekov7@gmail.com>
2026-02-03 15:17:06 +0500

    create

--- src/main/java/uz/raqamli_talim/oneedu/sevice/MyHemisService.java
+++ src/main/java/uz/raqamli_talim/oneedu/sevice/MyHemisService.java
@@ -67,7 +67,6 @@ public class MyHemisService {
                 pin = ui.getPin();
                 serial = ui.getPportNo();
             }
-
             authService.saveAudit(clientSystem, pin, serial, true, msg);
 
             return UriComponentsBuilder
